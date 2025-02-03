5ff516d4bd49c9164ab105f136c1973196341a0c
aroualid <aroualid@student.42.fr>
2025-02-03 10:07:23 +0100

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include  <cctype>
+
+int main (int ac, char **av)
+{
+	if (ac < 2) { 
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j] != '\0'; j++)
+		{
+			std::cout << (char)std::toupper(av[i][j]);
+		}
+		if (i < ac - 1)
+			std::cout << " ";
+	}
+	std::cout << "\n";
+}
